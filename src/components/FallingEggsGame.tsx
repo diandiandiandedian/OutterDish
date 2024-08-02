953d475db2e12e882a311a8a2aef8376fb98437c
zerooooooo1 <xushiduo1@gmail.com>
2024-08-02 22:00:22 +0800

    测试音频

--- src/components/FallingEggsGame.tsx
+++ src/components/FallingEggsGame.tsx
@@ -301,10 +301,10 @@ const FallingEggsGame: React.FC<{ fromLogin?: string }> = ({fromLogin}) => {
     };
 
     function playAddScore() {
-        // audioRef.current?.play();
+        audioRef.current?.play();
         if (!userStopBackground) {
             if (!isMusicPlaying) {
-                audioRef.current?.play();
+                // audioRef.current?.play();
                 setIsMusicPlaying(true)
             }
             // console.error('addScoreAudioRef.current?.audioEl.current', addScoreAudioRef.current?.audioEl.current)
