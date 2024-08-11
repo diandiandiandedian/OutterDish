814942099ace714c52ea76f413d3be27fb59969e
zerooooooo1 <xushiduo1@gmail.com>
2024-08-11 13:45:08 +0800

    play noton

--- src/components/QuestsPage.tsx
+++ src/components/QuestsPage.tsx
@@ -26,18 +26,18 @@ const QuestsPage: React.FC = () => {
                     </div>
                 </div>
                 <QuestItem
+                    imgSrc="/panter/noton.jpg"
+                    title="Use NOTON mine $NOT"
+                    description=""
+                    points={playgame}
+                    id="noton"
+                />
+                <QuestItem
                     imgSrc="/panter/TapPop.png"
                     title="Play TapPop & earn rewards"
                     description=""
                     points={playgame}
                     id="TapPop"
-                />
-                <QuestItem
-                    imgSrc="/chipigo.png"
-                    title="Play Chipigo"
-                    description="Play Outter‘s friend Chipigo‘s game"
-                    points={playgame}
-                    id="Chipigo"
                 />
                 <QuestItem
                     imgSrc="/tg.svg"
